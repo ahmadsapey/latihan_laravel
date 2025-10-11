9297425ffe85c8f62f833f826c843b619183f1f7
ahmadsapey <sapeyamad.lp3i.krw24@gmail.com>
2025-10-11 09:23:56 +0700

    membuat studenregister

---
+++ app/Http/Controllers/Auth/StudentRegisterController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class StudentRegisterController extends Controller
+{
+    //
+    public function showRegistrationForm()
+    {
+        return view('auth.register-mahasiswa');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'student', // Set role to student
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('student.dashboard') // Redirect to student dashboard
+        ->with('success', 'Registration successful. Welcome!');
+    }
+}
