fd6925e747974f57d7294c659020453c21f3232d
ahmadsapey <sapeyamad.lp3i.krw24@gmail.com>
2025-11-01 11:25:38 +0700

    + step1 datapribadi

--- resources/views/ekyc/step1.blade.php
+++ resources/views/ekyc/step1.blade.php
@@ -1,4 +1,11 @@
+
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Data Peribadi') }}
+        </h2>
+    </x-slot>
+
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
             <h2 class="text-2xl font-bold mb-4">Data Peribadi</h2>
